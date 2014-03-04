e849b0d86f8c3df1ad0c170ffb158fddea86fb67
Johannes Fürmann <johannes@weltraumpflege.org>
2014-03-04 01:01:50 +0100

    events are now sorted by starting date

--- labevents/views/events.py
+++ labevents/views/events.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from flask import g
 from flask import render_template, abort
 
@@ -7,10 +9,11 @@ from labevents.models import Event
 @app.route('/')
 def show_events():
     events = []
-    for e in g.db.query(Event).all():
+    for e in g.db.query(Event).filter(Event.start_date > date.today()).\
+        order_by(Event.start_date).all():
         events.extend(e.resolve_repetitions())
     return render_template("web_show_events.html",
-                           events=events)
+                           events=sorted(events, key=lambda e: e.start_date))
     
 @app.route('/events/<int:id>')
 def show_event_details(id):
