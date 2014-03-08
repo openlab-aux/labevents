99cc1579cf2afa09505c49cde24442b22e571756
Johannes Fürmann <johannes@weltraumpflege.org>
2014-03-08 02:33:09 +0100

    display regular events even if the original event is in the past

--- labevents/views/events.py
+++ labevents/views/events.py
@@ -4,6 +4,7 @@ from flask import g
 from flask import render_template, abort
 from flask import Response
 from html2text import html2text
+from sqlalchemy.orm import contains_eager
 import icalendar
 
 from labevents import app
@@ -11,10 +12,18 @@ from labevents.models import Event
 
 def __get_events(conn):
     events = []
-    for e in conn.query(Event).filter(Event.start_date > date.today()).\
-        order_by(Event.start_date).all():
-        events.extend(e.resolve_repetitions())
-    return sorted(events, key=lambda e: e.start_date)
+    for e in conn.query(Event).options(contains_eager('location'), 
+                                       contains_eager('owner')).\
+             filter((Event.start_date > date.today()) | 
+                    (Event.repetition_pattern != Event.REPETITION_NONE)).\
+             order_by(Event.start_date).all():
+        for r in e.resolve_repetitions():
+            if r.start_date.date() >= date.today():
+                events.append(r)
+    return sorted(
+        events,
+        key=lambda e: e.start_date
+    )
 
 
 @app.route('/')
